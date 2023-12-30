76ab907316378c6c0ba619aec040601ec8a6a5f4
yylykym <yylykym3@outlook.com>
2023-12-30 23:01:27 +0800

    layout

---
+++ UILayoutTest/app/src/main/java/com/yylykym/uilayouttest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yylykym.uilayouttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

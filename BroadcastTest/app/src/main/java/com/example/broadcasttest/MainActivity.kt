372100f9394d44d715adb195838c94a30cc44a5d
y t <yylykym3@outlook.com>
2024-01-04 17:08:30 +0800

    broadcast receiver

---
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.broadcasttest
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.broadcasttest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var timeChangeReceiver: TimeChangeReceiver
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.TIME_TICK")
+        timeChangeReceiver = TimeChangeReceiver()
+        registerReceiver(timeChangeReceiver, intentFilter)
+        mainBinding.button.setOnClickListener {
+            val intent = Intent("com.example.broadcasttest.MY_BROADCAST")
+            intent.setPackage(packageName)
+            sendOrderedBroadcast(intent, null)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(timeChangeReceiver)
+    }
+
+    inner class TimeChangeReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, p1: Intent?) {
+            Toast.makeText(context, "Time has changed", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}

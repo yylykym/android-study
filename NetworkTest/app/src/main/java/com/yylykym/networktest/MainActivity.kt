bee0238fd6f42f53120761d504f43622f4d35bd3
y t <yylykym3@outlook.com>
2024-01-18 15:29:32 +0800

    1

---
+++ NetworkTest/app/src/main/java/com/yylykym/networktest/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.yylykym.networktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yylykym.networktest.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.sendRequestBtn.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+    }
+
+    private fun sendRequestWithHttpURLConnection() {
+        // 开启线程发起网络请求
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                // 下面对获取到的输入流进行读取
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun showResponse(response: String) {
+        runOnUiThread {
+            // 在这里进行UI操作，将结果显示到界面上
+            binding.responseText.text = response
+        }
+    }
+}
